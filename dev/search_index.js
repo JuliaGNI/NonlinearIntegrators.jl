6e1c4dc7e4fd6c0936105166d879c9d4832fcffb
Documenter.jl <documenter@juliadocs.github.io>
2024-02-21 08:20:01 +0000

    build based on 8005f64

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NonlinearIntegrators","category":"page"},{"location":"#NonlinearIntegrators","page":"Home","title":"NonlinearIntegrators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NonlinearIntegrators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NonlinearIntegrators]","category":"page"}]
+}
