0bcfa81728dfd43dc561ee8a48b2b5a9ad57fcdd
ZeyuanLee <zeyuanli.tum@outlook.com>
2024-11-26 10:56:52 +0100

    add plots for wave equation.

--- src/classical_methods/test_linear_wave.jl
+++ src/classical_methods/test_linear_wave.jl
@@ -7,7 +7,13 @@ using Plots
 problem = LinearWave.hodeproblem(tspan = (0, 20), tstep = 0.005)
 sol = integrate(problem, RK4())
 
+
 @gif for i in 1:1000
     plot(sol.q[i, :],ylims=(-3,3))
 end
+
+xs = -0.5:1/(257):0.5
+ts = 0:0.005:0.02
+surface(xs,ts,hcat(sol.q[1:5, :]...)',xlabel = "space", ylabel = "time ")
+
 
